505b542a4f50481014dd0928c0d03c7fd864a7b2
ferryal <ferryal.bahari@amartha.com>
2020-05-22 06:02:59 +0700

    change routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ListSpace from './schenes/List-Space';
 import DetailSpace from './schenes/Detail-Space';
-import config from './config';
-
 
 class Routes extends Component {
   render() {
     return (
         <Switch>
-          <Route exact path={`${config.mainPath}/list-space`} component={ListSpace} />
-          <Route path={`${config.mainPath}/detail-space/:id`} component={DetailSpace} />
-          <Redirect to={`${config.mainPath}/list-space`} />
+          <Route exact path="/space" component={ListSpace} />
+          <Route path="/space-detail/:id" component={DetailSpace} />
         </Switch>
     );
   }
